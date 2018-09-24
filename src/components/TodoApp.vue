e4e780128dcb7c0ed4935933117f98002fa4857e
kian00sh <kianoosh.geravesh@itggot.se>
2018-09-24 15:14:43 +0200

    first commit

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+    ol.list
+        li(v-for="(item,index) in items" @click="check(index)") 
+            | {{ index + ". " + item }}
+        input(type="text" v-model="newItem" @keyup.enter="itemAdd")
+        button(@click="itemAdd") Add
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data: ()=>({
+        items: new Array,
+        newItem: "",
+    }),
+    methods: {
+        itemAdd: function () {
+            this.items.push(this.newItem);
+            this.newItem = "";
+        },
+        check: function (index:number) {
+            this.items = this.items.filter((item, i) => {
+                return i != index;
+            });
+        }
+    },
+    name: 'TodoApp'
+});
+</script>
+
+<style scoped lang="scss">
+    ol {
+        list-style-type: none;
+    }
+    ol:hover {
+        cursor: pointer;
+    }
+</style>
